6946aedfc7ce123fb44cf2f715fdafa9c1b79401
Aniruddha Thorat <aniruddhathorat123@gmail.com>
2019-08-29 00:37:43 +0530

    recycler view remaining ...

--- app/src/main/java/com/aniruddha/writerapp/FileListAdapter.kt
+++ app/src/main/java/com/aniruddha/writerapp/FileListAdapter.kt
@@ -1,30 +1,47 @@
 package com.aniruddha.writerapp
-import android.view.View
 import android.content.Context
+import android.system.Os.bind
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.file_list_item.view.*
+import java.io.File
 
-class FileListAdapter(val items : ArrayList<String>,val context: Context):
-    RecyclerView.Adapter<FileListAdapter.MyAdapter>(){
+class FileListAdapter(private val items : ArrayList<String>,val context: Context):
+    RecyclerView.Adapter<FileListAdapter.ViewHolder>(){
 
-    class MyAdapter(val filename : TextView):RecyclerView.ViewHolder(filename)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter {
-        return MyAdapter(LayoutInflater.from(context)
-            .inflate(R.layout.file_list_item,parent,false) as TextView)
+    private lateinit var listener : OnFileNameClickListener
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.file_list_item,
+            parent,
+            false
+            )
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: MyAdapter, position: Int) {
-        holder.filename.text = items[position]
+    fun setClickListener(listener: OnFileNameClickListener){
+        this.listener = listener
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int){
+        holder.bind(position,items[position])
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        fun bind(position: Int,data: String){
+            itemView.fileItem.text = data
+            
+        }
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
-}
+    interface OnFileNameClickListener{
+        fun onFileNameClick(position : Int)
+    }
 
-interface ItemClickListener{
-    fun setOnClickListener(position : Int)
 }
--- app/src/main/java/com/aniruddha/writerapp/FilesListFragment.kt
+++ app/src/main/java/com/aniruddha/writerapp/FilesListFragment.kt
@@ -8,15 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import java.util.ArrayList
 
-class FilesListFragment : Fragment() {
-    companion object{
-        lateinit var list : ArrayList<String>
+class FilesListFragment : Fragment(),FileListAdapter.OnFileNameClickListener {
+    companion object {
+        lateinit var list: ArrayList<String>
 
-        fun getInstance(bundle: Bundle):FilesListFragment{
-            list = bundle.getStringArrayList(WriterConstants.FILES_LIST)
-            return FilesListFragment()
-        }
+        fun getInstance(list: String)
     }
+
+    private lateinit var recyclerView :FileListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +23,12 @@ class FilesListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_files_list, container, false)
+
+
+    }
+
+    override fun onFileNameClick(position : Int){
+
     }
 }
 
